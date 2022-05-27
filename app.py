ff09e1f5e8a1651fdfa589c6cfeea44c661e25b6
Rimottie <Rimottie@gmail.com>
2022-05-27 21:04:23 +0000

    Atualiza para uma aplicação web usando Flask

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+# Ao abrir o GitPo, excute:
+#pip install -r requirements.txt
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World'
+
+app.run()
+
 print('Olá Mundo')
